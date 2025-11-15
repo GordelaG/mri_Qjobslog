bd55aeb819ce20b9e4dc39324b009f8a8c9ec8b0
Gordela <ailtonplay2700@gmail.com>
2025-11-15 09:20:41 -0300

    Update cfg.lua
    Removido Webhook de teste

--- shared/config.lua
+++ shared/config.lua
@@ -2,4 +2,4 @@ Config = Config or {}
 Config.Core = Config.Core or 'qb-core'  -- ou 'qbx-core' conforme seu servidor
 
 -- Webhook central de STAFF (exclusivo p/ relatórios STAFF)
-Config.StaffWebhook = "https://discord.com/api/webhooks/1406344875107946626/kMUpqW8ceEcmhQp0EMJ08qIVDbyuKqHnpfMCsCIqlkPB7YolRo7iwpX6ytgr3CAMCrGq"
+Config.StaffWebhook = "ADD_STAFFWEBHOOK_HERE"
